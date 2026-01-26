92a51aa9acdf725d11ae3c59fdc8b367257d7ef4
Tirth Rojara <work.tirthrojara@gmail.com>
2026-01-26 10:10:21 +0530

    Update: proxy.ts => add role base access controll in route

--- proxy.ts
+++ proxy.ts
@@ -1,31 +1,37 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
+import store from "./store/index.store";
 
 // This function can be marked `async` if using `await` inside
 export function proxy(req: NextRequest) {
     const path = req.nextUrl.pathname;
     const token = req.cookies.get("__secure-rtk")?.value;
-    const role = req.cookies.get("role")?.value;
+    const role = req.cookies.get("role")?.value?.toLowerCase();
 
-    if (
-        path.startsWith("/_next") ||
-        path.startsWith("/favicon") ||
-        path.includes(".")
-    ) {
+    if (path.startsWith("/_next") || path.startsWith("/favicon") || path.includes(".")) {
         return NextResponse.next();
     }
 
-    const isPublicPath =
-        path === "/login" ||
-        path === "/signup" ||
-        path.startsWith("/forgotpassword");
+    const isPublicPath = path === "/login" || path === "/signup" || path.startsWith("/forgotpassword");
 
+    // uncomment in production
     // if (!isPublicPath && !token) {
     //     return NextResponse.redirect(new URL("/login", req.url));
     // }
-
-    if(!token) {
+    if (!token) {
         return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    if (path.startsWith("/dashboard")) {
+        // If the path includes "candidate" but role is NOT candidate
+        if (path.includes("/candidate") && role !== "candidate") {
+            return NextResponse.redirect(new URL("/dashboard/recruiter", req.url));
+        }
+
+        // If the path includes "recruiter" but role is NOT recruiter
+        if (path.includes("/recruiter") && role !== "recruiter") {
+            return NextResponse.redirect(new URL("/dashboard/candidate", req.url));
+        }
     }
 
     if (token && path === "/login") {
