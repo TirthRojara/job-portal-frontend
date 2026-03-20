a90e937a15b7249e695b90fa9ebc4994a32506d2
Tirth Rojara <work.tirthrojara@gmail.com>
2026-03-20 11:46:36 +0530

    Update:UI: add login button in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,27 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className=" flex flex-col md:flex-row gap-10 items-center justify-center mt-40 ">
+        <div className="flex flex-col items-center min-h-screen gap-10">
+            <main className=" flex flex-col md:flex-row gap-10 items-center justify-center mt-40 ">
+                <div>
+                    <h2 className=" text-red-500 text-3xl">Recruiter Id & password</h2>
+                    <p className=" text-green-500 text-2xl">Email: test2@gmail.com</p>
+                    <p className=" text-green-500 text-2xl">Password: test1234</p>
+                </div>
+                <div>
+                    <h2 className=" text-red-500 text-3xl">Candidate Id & password</h2>
+                    <p className=" text-green-500 text-2xl">Email: test1@gmail.com</p>
+                    <p className=" text-green-500 text-2xl">Password: test1234</p>
+                </div>
+            </main>
             <div>
-                <h2 className=" text-red-500 text-3xl">Recruiter Id & password</h2>
-                <p className=" text-green-500 text-2xl">Email: test2@gmail.com</p>
-                <p className=" text-green-500 text-2xl">Password: test1234</p>
+                <Link href="/login">
+                    <Button>Login</Button>
+                </Link>
             </div>
-            <div>
-                <h2 className=" text-red-500 text-3xl">Candidate Id & password</h2>
-                <p className=" text-green-500 text-2xl">Email: test1@gmail.com</p>
-                <p className=" text-green-500 text-2xl">Password: test1234</p>
-            </div>
-        </main>
+        </div>
     );
 }
