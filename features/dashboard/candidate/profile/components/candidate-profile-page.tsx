1d303cf9519aa31d019a856d7762dcc2cd9cb8bd
Tirth Rojara <work.tirthrojara@gmail.com>
2026-03-21 18:43:10 +0530

    Fix:Bug: in candidate profile handing ui state

--- features/dashboard/candidate/profile/components/candidate-profile-page.tsx
+++ features/dashboard/candidate/profile/components/candidate-profile-page.tsx
@@ -13,37 +13,25 @@ import CreateProfile from "./create-profile";
 import { useGetCandidateProfilById, useGetCandidateProfile } from "../api/query";
 import { useAppSelector } from "@/store/index.store";
 
-
-// const profile = {
-//     name: "John Smith",
-//     role: "Senior Frontend Developer",
-//     location: "San Francisco, CA",
-//     email: "john.smith@email.com",
-//     completion: 85,
-// };
-
-// const languages = [
-//     { id: "1", name: "English", level: "Native" },
-//     { id: "2", name: "Spanish", level: "Beginner" },
-//     { id: "3", name: "Gujarati", level: "Intermediate" },
-// ];
-
 export default function CandidateProfilePage() {
-    const role = useAppSelector((state)=> state.app.role)
+    const role = useAppSelector((state) => state.app.role);
 
     const { data: userData, isError, isLoading } = useGetUserData();
     const { data: candidateData, isLoading: isLoadingCandidateData } = useGetCandidateProfile(role);
 
     console.log("candidate data", userData);
 
-    if (isLoading) return <></>;
-    if (isLoadingCandidateData) return <></>;
+    if (isLoading || isLoadingCandidateData) return <></>;
+    if (!userData?.data) return <></>;
+
+    const isProfileReady = !!userData?.data?.candidateProfileId;
 
     return (
         <>
             <div className="flex flex-col justify-centers items-center gap-6 px-4 py-6">
-                {!userData?.data?.candidateProfileId && <CreateProfile />}
-                {userData?.data?.candidateProfileId && (
+                {!isProfileReady ? (
+                    <CreateProfile />
+                ) : (
                     <>
                         {/* <ProfileSummaryCard profile={profile} /> */}
                         <Summary summary={candidateData?.data?.summary!} />
